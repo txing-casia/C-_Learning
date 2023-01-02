32c17d8a4330cce230da9248677ccb1f00a25bf4
Junjie Zhou <yuxue0126@126.com>
2023-01-02 15:56:14 +0800

    5

--- Chapter 5 Statement/homework/test.cpp
+++ Chapter 5 Statement/homework/test.cpp
@@ -2,16 +2,24 @@
 #include <vector>
 using namespace std;
 
+void change(double a[3])
+{
+    a[0] = 1.0;
+    a[1] = 2.0;
+    a[2] = 3.0;
+}
+
 int main(void)
 {
     vector<char> A = {1,2,3,'a','b','x','z'};
     vector<char> B = {1,2,2,3,'a','b','x','z'};
-    // if (A.size()>B.size())
-    //     cout<<A.size()<<endl;
-    // else if (A.size()<=B.size())
-    //     cout<<A.size()<<endl;
-    char C = 'Z';
-    cout<<char(C+32);
+    double out[3]{0.0,0.0,0.0};
+    change(out);
+    for(int i=0;i<3;i++)
+    {
+        cout << out[i] << endl;
+    }
+
 
     return 0;
 }
