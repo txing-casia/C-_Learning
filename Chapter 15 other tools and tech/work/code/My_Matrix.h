ae3956ee54e3d79f30e4508f2d453bbe6bc2862a
Txing <yuxue0126@126.com>
2023-04-23 20:54:38 +0800

    update

---
+++ Chapter 15 other tools and tech/work/code/My_Matrix.h
@@ -0,0 +1,16 @@
+class My_Matrix
+{
+    My_Matrix()
+    {
+        x = 0;
+    }
+
+public:
+
+    int x;
+
+
+
+};
+
+
---
+++ Chapter 15 other tools and tech/work/code/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <My_Matrix.h>
+
+My_Matrix<int> mat1;
+
+
+
+
+
