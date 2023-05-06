3e5d8fd6864ea5292aafd9f9c8fd0bbd8bb6aa5b
Txing <yuxue0126@126.com>
2023-05-07 01:13:34 +0800

    update

--- Chapter 15 other tools and tech/work/code/head.h
+++ Chapter 15 other tools and tech/work/code/head.h
@@ -1,80 +1,134 @@
 #include <iostream>
 #include <vector>
 #include <span>
+#include <initializer_list>
 template <typename T>
 class My_Matrix
 {
-    // friend auto operator + (My_Matrix A, My_Matrix B)
-    // {
-    //     My_Matrix res;
-    //     res.x = A.x + B.x;
-    //     return res;
-    // }
+    friend My_Matrix operator + (const My_Matrix& A, const My_Matrix& B)
+    {
+        // ref: 加法实现参考助教老师代码
+        if ((A.i != B.i) || (A.j != B.j))
+        {
+            std::cout << "std::length_error(My_Matrix::operator +, length does not match)" << std::endl;
+            throw std::length_error("My_Matrix::operator +, length does not match");
+        }
+        My_Matrix ret(A.i, A.j);
+        const uint16_t len = A.i * A.j;
+        for (auto i = 0; i < len; ++i)
+            ret.data[i] = A.data[i] + B.data[i];
+        return ret;
+    }
+
+    friend My_Matrix operator - (const My_Matrix& A, const My_Matrix& B)
+    {
+        // ref: 减法实现参考助教老师代码
+        if ((A.i != B.i) || (A.j != B.j))
+        {
+            std::cout << "std::length_error(My_Matrix::operator -, length does not match)" << std::endl;
+            throw std::length_error("My_Matrix::operator -, length does not match");
+        }
+        My_Matrix ret(A.i, A.j);
+        //cant use data.len due to push_back
+        const uint16_t len = A.i * A.j;
+        for (auto i = 0; i < len; ++i)
+            ret.data[i] = A.data[i] - B.data[i];
+        return ret;
+    }
+
+
 
 public: // private:
     T x;
     std::vector<T> data;
     int i;
     int j;
+    int length = i * j;
     // 无参数初始化
-    My_Matrix():x(0), i(1), j(1), data(1, x){}
+    My_Matrix():x(0), i(1), j(1), length(1), data(1, x){}
     // 输入矩阵尺寸初始化
-    My_Matrix(int m, int n): x(0), i(m), j(n) ,data(m * n, x){}
-
-    // std::span<T> operator[](int row)
-    // {
-    //     const auto beginIt = data.begin() + row * j;
-    //     return std::span(beginIt, j);
-    // }
-
-
-    typename std::vector<T>::iterator operator [](std::size_t idx)
+    My_Matrix(int m, int n): x(0), i(m), j(n),length(m * n) ,data(m * n, x){}
+    // initializer_list初始化
+    My_Matrix(std::initializer_list<T> list) : data(list), i(1), j(list.size()), length(list.size())
     {
-        /*
-        ref: [][]访问功能参考助教老师的代码示例
-        */
+        // ref: initializer_list初始化功能参考助教老师的代码示例
+        if (list.size() == 0)
+        *this = My_Matrix();
+    }
+
+    typename std::vector<T>::iterator operator [](const std::size_t idx)
+    {
+        // ref: [][]访问功能参考助教老师的代码示例
         if(idx > (i - 1)){
+            std::cout << "My_Matrix::operator[], access invalid row!" << std::endl;
             throw std::range_error("My_Matrix::operator[], access invalid row!");
         }
         return data.begin() + (idx * j);
     }
 
-    T at(std::size_t idx)
+    auto& at(const std::size_t idx)
     {
-        if(idx > j * i - 1){
+        if(idx > length - 1){
+            std::cout << "My_Matrix::operator[], access invalid row!" << std::endl;
             throw std::range_error("My_Matrix::operator[], access invalid row!");
         }
         return data[idx];  // data[idx] ;
     }
 
+    void push_back(const int input)
+    {
+        data.push_back(input);
+        length++;
+    }
 
-    My_Matrix& My_Matrix::operator=(const My_Matrix &c)
-    {//具备自我赋值安全性和备异常安全性
-        A *temp=p;
-        p=new A(*c.p);
-        delete temp;
-        return *this;
+    void reshape(const int A, const int B)
+    {
+        if (A * B <= length)
+        {
+            for(int i = A * B; i < length; i++)
+            {
+                data.erase(data.begin() + A * B);
+            }
+        }else
+        {
+            for(int i = length; i < A * B; i++)
+            {
+                data.push_back(x);
+            }
+        }
+        i = A;
+        j = B;
+        length = i * j;
+    }
+
+    My_Matrix operator * (const My_Matrix& A)
+    {
+        // ref: 减法实现参考助教老师代码
+        if ((this->i != A.j) || (this->j != A.i))
+        {
+            std::cout << "std::length_error(My_Matrix::operator *, length does not match)" << std::endl;
+            throw std::length_error("My_Matrix::operator *, length does not match");
+        }
+        int ii = this->i;
+        int jj = A.j;
+        My_Matrix ret(ii, jj);
+        //cant use data.len due to push_back
+        const uint16_t len = A.i * A.j;
+        for (auto i = 0; i < ii; ++i)
+        {
+            for (auto j = 0; j < jj; ++j)
+            {
+                for (auto k = 0; k < A.i; ++k)
+                {
+                    //debug use
+                    //std::cout << this->getVal(i, k) << ' ' << m.getVal(k, j) << std::endl;
+                    ret[i][j] += (this->data[i * k]) * (A.data[k * j]);
+                }
+            }
+        }
+        return ret;
     }
 
 };
-
-// template <>
-// struct My_Matrix<int>
-// {
-
-//     std::span<int> operator [](int row)
-//     {
-//         const auto beginIt = data.begin() + row * j;
-//         return std::span(beginIt, j)
-//     }
-
-// };
-
-
-// template
-// class My_Matrix<int>()
-// {
-// };
-
 
 
--- Chapter 15 other tools and tech/work/code/main.cpp
+++ Chapter 15 other tools and tech/work/code/main.cpp
@@ -3,7 +3,20 @@
 #include <cassert>
 using namespace std;
 
-
+void print_a_matrix(My_Matrix<int> mat)
+{
+    for(int m = 0; m < mat.i; m++)
+    {
+        for(int n = 0; n < mat.j; n++)
+        {
+            cout << mat.at(n + m*mat.j) << " ";
+            if (n == mat.j-1)
+            {
+                cout << endl;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -12,25 +25,92 @@ int main()
         My_Matrix<int> mat1;
         cout << "1. 无参构造:" << endl;
         cout << "m x n size: " << size(mat1.data) << ", value: " << mat1.data.at(0) << endl;
+        cout << endl;
 
         My_Matrix<int> mat2(2,2);
         cout << "2. 输入矩阵尺寸构造:" << endl;
         cout << "m x n size: " << size(mat2.data) << ", value: " << mat2.data.at(0) << endl;
+        print_a_matrix(mat2);
+        cout << endl;
 
         mat2[1][0] = 42;
         cout << "3. 通过[][]访问内部元素" << endl;
         cout << "mat2[1][0]: " << mat2[1][0] << endl;
+        print_a_matrix(mat2);
+        cout << endl;
 
         cout << "4. 使用at访问矩阵按行展开后对应的元素:" << endl;
         cout << "mat2.at(2): " << mat2.at(2) << endl;
+        cout << "assert(mat2.at(2) == 42);" << endl;
+        assert(mat2.at(2) == 42);
+        cout << endl;
 
         cout << "5. at访问越界时抛出异常: (取消注释可查看)" << endl;
         // cout << "mat2.at(5): " << mat2.at(5) << endl;
+        cout << endl;
 
-        mat2.at(1);
+        mat2.at(1) = 1;
         cout << "6. 可用at实现赋值 mat2.at(1) = 1:" << endl;
-        cout << mat2.at(1) << endl;
+        cout << "mat2.at(1): "<< mat2.at(1) << endl;
+        cout << endl;
 
+        mat2.push_back(13);
+        cout << "7. 使用push_back()向Matrix添加元素，可以用at访问" << endl;
+        cout << "mat2.push_back(13); mat2.at(4): " << mat2.at(4) << endl;
+        cout << "assert(mat2.at(4) == 13);" << endl;
+        assert(mat2.at(4) == 13);
+        print_a_matrix(mat2);
+        mat2.push_back(14);
+        cout << "mat2.push_back(14); mat2.at(5): " << mat2.at(5) << endl;
+        cout << "assert(mat2.at(5) == 14);" << endl;
+        assert(mat2.at(5) == 14);
+        print_a_matrix(mat2);
+        cout << endl;
+
+
+        cout << "8. 使用reshape()修改矩阵尺寸，多余的元素会被删除，缺少的元素会使用默认值填充:" << endl;
+        mat2.reshape(3, 3);
+        cout << "mat2.reshape(3, 3);" << endl;
+        cout << "mat2.length: " << mat2.length << ", mat2.i: " << mat2.i << ", mat2.j: " << mat2.j <<  endl;
+        print_a_matrix(mat2);
+        mat2.reshape(1, 3);
+        cout << "mat2.reshape(1, 3);" << endl;
+        cout << "mat2.length: " << mat2.length << ", mat2.i: " << mat2.i << ", mat2.j: " << mat2.j << endl;
+        print_a_matrix(mat2);
+        cout << endl;
+
+        My_Matrix<int> mat3{1, 2, 3, 4};
+        My_Matrix<int> mat4{5, 6, 7, 8};
+        cout << "9. 使用initializer_list构造Matrix，此时Matrix为一个行向量" << endl;
+        mat3.reshape(2, 3);
+        cout << "mat3.reshape(2, 3);" << endl;
+        cout << "mat3.length: " << mat3.length << ", mat2.i: " << mat3.i << ", mat2.j: " << mat3.j <<  endl;
+        print_a_matrix(mat3);
+        mat4.reshape(2, 3);
+        cout << "mat4.reshape(2, 3);" << endl;
+        cout << "mat4.length: " << mat4.length << ", mat2.i: " << mat4.i << ", mat2.j: " << mat4.j <<  endl;
+        print_a_matrix(mat4);
+        cout << endl;
+
+        cout << "10. 矩阵加法，如果矩阵尺寸不同需抛出异常:" << endl;
+        cout << "mat3 + mat4;" << endl;
+        print_a_matrix(mat3 + mat4);
+        cout << endl;
+
+        cout << "11. 矩阵减法，如果矩阵尺寸不同需抛出异常:" << endl;
+        cout << "mat3 - mat4;" << endl;
+        print_a_matrix(mat3 - mat4);
+        cout << endl;
+
+        cout << "12. 矩阵乘法，此处为两个2*3的矩阵相乘，不满足矩阵乘法定义，需要抛出异常:" << endl;
+        cout << "mat3 * mat4; // 抛出异常（取消下一行的注释可查看异常）" << endl;
+        // print_a_matrix(mat3 * mat4);
+        cout << endl;
+
+        cout << "13. 将 mat4 reshape 之后执行mat3 * mat4：" << endl;
+        mat4.reshape(3,2);
+        print_a_matrix(mat3 * mat4);
+        cout << endl;
 
 
     }
